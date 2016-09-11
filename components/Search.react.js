08c8d84e6fbef881c652cbf8d0ff364b56006027
Jimmy Chou <cjimmy@ca.ibm.com>
2016-09-11 00:00:37 -0400

    loading svg added when searching

--- components/Search.react.js
+++ components/Search.react.js
@@ -2,11 +2,13 @@
 
 var React = require('react');
 var Suggestions = require('./Suggestions.react.js');
+var Loader2 = require("./Loader2.react.js");
 
 module.exports = Search = React.createClass({
     getInitialState:function(){
       return {
           searchInput:'',
+          searching:false,
           cachedSearch:{}
       }  
     },
@@ -16,9 +18,16 @@ module.exports = Search = React.createClass({
     },
     handleChange:function(event){
         console.log(event.target.value);
+        var self = this;
         this.setState({
-            searchInput:event.target.value
+            searchInput:event.target.value,
+            searching:true
         });
+        setTimeout(function(){
+            console.log('loadgin');
+        // Set application state (Not paging, add tweets)
+            self.setState({searching:false});
+      }, 1000);
         // console.log(this.props.searchInput);
     },
     processEnter:function(event) {
@@ -33,6 +42,7 @@ module.exports = Search = React.createClass({
         <h4>Search for tweets</h4>
             <input type = 'text' onKeyPress={this.processEnter} defaultValue = "Input keyword to search" value = {this.state.searchInput} onChange={this.handleChange}/>
             <button onClick = {this.handleClick}>Search</button>
+            <Loader2 paging = {this.state.searching}/>
             <Suggestions suggest={this.state.searchInput}/>
         </div>
         )
